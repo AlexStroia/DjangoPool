0b703eeabd74d4ed4f202150a81783db6e3ed403
Alexandru Stroia <alexandru.stroia@halcyonmobile.com>
2019-12-21 19:58:38 +0200

    Add Models for the Pool App

--- pool/polls/models.py
+++ pool/polls/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.db import models
 
 from django.db import models
 
@@ -11,8 +10,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("Date published")
 
+    def __str__(self):
+        return self.question_text
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        self.choice_text
